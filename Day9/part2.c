a0a2dda3c9a0109d0cc2e19999c34a27fdb38ab6
akbarjon2000 <1543431a@gmail.com>
2024-12-20 15:49:54 +0900

    cleaned code

--- Day9/part2.c
+++ Day9/part2.c
@@ -20,7 +20,6 @@ int main(){
     char line[MAX_LENGTH];
     fgets(line, sizeof(line), input);
     fclose(input);
-    // printf("%s\n", line);
 
     //Task2: Represent individual blocks
 
@@ -53,57 +52,40 @@ int main(){
         }
     }
 
-    // for(int i = 0; i < count; i++){
-    //     printf("%s", disk_map[i]);
-    // }
-
     // //move file blocks one at a time:
 
     int file_count = 1;
-    // int current_index = 0;
     int condition = 1;
-    // printf("%d\n", count);
     for(int j = count-1; j > 0; j--){
         if(strcmp(disk_map[j], ".") == 0) continue;
         if(strcmp(disk_map[j], disk_map[j-1]) == 0 && strcmp(disk_map[j], ".") != 0){
             file_count++;
             continue;
         }else{
-        // printf("%s: %ld\n", disk_map[j], file_count);
-            // printf("%d: ", file_count);
-        for(int i = 0; i < j; i++){
-            // printf("%d", i);
-            if(strcmp(disk_map[i], ".") == 0){
-                for(int k = i; k < i+file_count; k++){
-                    if(strcmp(disk_map[k], ".") != 0){
-                        condition = 0;
+            for(int i = 0; i < j; i++){
+                if(strcmp(disk_map[i], ".") == 0){
+                    for(int k = i; k < i+file_count; k++){
+                        if(strcmp(disk_map[k], ".") != 0){
+                            condition = 0;
+                            break;
+                        }
+                    }
+                    if(condition == 1){
+                        while(file_count > 0){
+                            free(disk_map[i]);
+                            disk_map[i] = (char *)malloc(strlen(disk_map[j+file_count-1]) + 1);
+                            strcpy(disk_map[i], disk_map[j+file_count-1]);
+                            strcpy(disk_map[j+file_count-1], ".");
+                            i++;
+                            file_count--;
+                        }
                         break;
                     }
+                    condition = 1;
                 }
-                // printf(" cond: %d ", condition);
-                if(condition == 1){
-                    while(file_count > 0){
-                        free(disk_map[i]);
-                        disk_map[i] = (char *)malloc(strlen(disk_map[j+file_count-1]) + 1);
-                        strcpy(disk_map[i], disk_map[j+file_count-1]);
-                        strcpy(disk_map[j+file_count-1], ".");
-                        // printf("%s is moved\n", disk_map[j]);
-                        i++;
-                        file_count--;
-                        // current_index++;
-                    }
-                    break;
-                }
-                condition = 1;
             }
         }
-        // printf("\n");
-        }
         file_count = 1;
-    }
-    printf("\n");
-    for(int i = 0; i < count; i++){
-        printf("%s", disk_map[i]);
     }
 
     // checksum algorithm:
@@ -114,7 +96,6 @@ int main(){
         if(strcmp(disk_map[i], ".") != 0){
             num = atoi(disk_map[i]);
             check_sum += i * num;
-            // printf("%s", disk_map[count]);
         }
     }
 
